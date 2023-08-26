8635d5937376bb97eb14cb340ed430009ab0f3cd
Jacoberson <c.jacob.moore92@gmail.com>
2023-08-26 18:26:09 -0500

    Do exercise 5 of js exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+    let sum = 0;
 
+    if ((number1 < 0 || number2 < 0) ||
+        (typeof (number1) !== 'number' || typeof (number2) !== 'number')) {
+        sum = 'ERROR';
+    } else if (number1 < number2) {
+        while (number1 <= number2) {
+            sum += number1;
+            number1++;
+        }
+    } else {
+        while (number1 >= number2) {
+            sum += number1;
+            number1--;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
