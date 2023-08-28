b2c70c1628cd26a2500320781c832d217b30b900
Jacoberson <c.jacob.moore92@gmail.com>
2023-08-28 16:21:24 -0500

    Do exercise 10 of js exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciNumber) {
+    if (fibonacciNumber < 0) {
+        return 'OOPS';
+    }
+    let prev2 = 0;
+    let prev1 = 1;
+    let curr;
 
+    for (let i = 1; i <= fibonacciNumber; i++) {
+        curr = prev2 + prev1;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev2;
 };
 
 // Do not edit below this line
