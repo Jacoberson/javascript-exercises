567d80e5e8c21239fd3784cf284b832a5846605d
Jacoberson <c.jacob.moore92@gmail.com>
2023-08-28 15:54:04 -0500

    Do exercise 9 of js exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (givenString) {
+    const re = /[\W_]/g;
+    givenString = givenString.toLowerCase().replace(re, '');
 
+    const reverseString = givenString.split('').reverse().join('');
+
+    return reverseString === givenString;
 };
 
 // Do not edit below this line
