2ec62523124a6aca9494cbb15f2d66e0e0ff039a
Jacoberson <c.jacob.moore92@gmail.com>
2023-08-28 15:41:49 -0500

    Do exercise 8 of js exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let runningTotal = 1;
+
+  while (num > 1) {
+    runningTotal *= num;
+    num--;
+  }
+
+  return runningTotal;
 };
 
 // Do not edit below this line
